497ad623e8732fdb6055970d8ea2bffddeffac24
Jakub Pietrus <pietrusjakub@gmail.com>
2024-03-05 16:51:53 +0100

    ✨ Refactor editor module and add Terminal struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![warn(clippy::all, clippy::pedantic)]
 mod editor;
-
+mod terminal;
 use editor::Editor;
+pub use terminal::Terminal;
+pub use editor::Position;
 
 fn main() {
-    let editor = Editor::default();
-    editor.run();
+    Editor::default().run();
 }
